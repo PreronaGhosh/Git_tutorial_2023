b961380a64360d0579a99ff7d60198b0dbe24e36
Prerona <preronaghosh1997@gmail.com>
2023-01-14 17:54:06 -0500

    New changes added again.

--- file1.py
+++ file1.py
@@ -2,4 +2,5 @@ def main():
     print "Hello World! Welcome to a Python code."
 
 
-
+def add(a, b):
+   return a + b
--- file2.cpp
+++ file2.cpp
@@ -8,3 +8,10 @@ void func(int a)
 {
    printf("This is a new function!")
 }
+
+void add(int a, int b)
+{
+   res = a+b;
+   cout<<"The sum is: ";
+   cout<<res;
+}
